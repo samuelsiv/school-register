d6d4208cebbab1a4abe3d88345bba2565a02f6c1
Samuel Siviero <andrehplayah@gmail.com>
2025-05-11 22:58:30 +0200

    feat: add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,18 @@ import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 
 import dotenv from 'dotenv';
+import { cors } from 'hono/cors';
 dotenv.config();
 
 const app = new Hono(); 
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!');
-})
+app.use('*', cors({
+  origin: '*', // this is for development only, change it to your domain in production
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Authorization', 'Content-Type'],
+  exposeHeaders: ['Authorization'],
+  maxAge: 600,
+}));
 
 app.route("", await (await import("./routes/admin/create-account.js")).default());
 app.route("", await (await import("./routes/auth/login.js")).default());
