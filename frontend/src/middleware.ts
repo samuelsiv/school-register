064458e224d7beda2931c70fe2bdbd36f4768dfa
Samuel Siviero <andrehplayah@gmail.com>
2025-05-12 11:33:33 +0200

    clean code

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,17 +1,18 @@
-import {NextFetchEvent, NextResponse} from 'next/server'
+import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import {isLogged} from "@/lib/auth";
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-    const loginUrl = request.nextUrl.clone()
-    loginUrl.pathname = '/login'
-    const logged = await isLogged(request)
-    if (!logged) return NextResponse.redirect(loginUrl)
-    else return NextResponse.next()
+    if (!await isLogged(request)) {
+        const loginUrl = request.nextUrl.clone();
+        loginUrl.pathname = '/login';
+
+        return NextResponse.redirect(loginUrl)
+    }
+
+    return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
     matcher: ["/home"],
 }
