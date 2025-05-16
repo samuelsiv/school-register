ed334537c5c9053b455cfc7c0792d54692619d8f
Samuel Siviero <andrehplayah@gmail.com>
2025-05-16 20:06:24 +0200

    fix: auth.ts broken

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -9,19 +9,18 @@ export const isLogged = async (
   role: "student" | "parent" | "teacher" | "admin" | null;
 	authenticated: boolean;
 } | null> => {
-    const token = request.cookies.get("auth_token")?.value;
-    if (!token) return false;
-    try {
-        const data = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET || ""));
-        return {
-						role: data.payload.role as "student" | "parent" | "teacher" | "admin",
-						authenticated: true,
-				};
-    } catch (e) {
-        return {
-						role: null,
-						authenticated: false,
-				};
-			}
-    }
-};
+  const token = request.cookies.get("auth_token")?.value;
+  if (!token) throw new Error("No token found");
+  try {
+    const data = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET || ""));
+    return {
+			role: data.payload.role as "student" | "parent" | "teacher" | "admin",
+			authenticated: true,
+		};
+  } catch (e) {
+    return {
+			role: null,
+			authenticated: false,
+		};
+	}
+}
