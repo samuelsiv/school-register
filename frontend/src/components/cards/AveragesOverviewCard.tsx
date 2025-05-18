b7af30019a4ea0edd54f45908e140e7f5ea8a588
Christian <25307474+Chicchi7393@users.noreply.github.com>
2025-05-18 18:33:24 +0200

    more and more animations

--- frontend/src/components/cards/AveragesOverviewCard.tsx
+++ frontend/src/components/cards/AveragesOverviewCard.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {ArrowLeftIcon} from "lucide-react"
+import {ArrowLeftIcon, ChevronRight} from "lucide-react"
 
 import {Card, CardContent, CardDescription, CardHeader, CardTitle,} from "@/components/ui/card"
 
@@ -9,6 +9,7 @@ import {Gauge} from "@/components/ui/gauge";
 import AverageCard from "@/components/cards/AverageCard";
 import {useState} from "react";
 import GradeCard from "@/components/cards/GradeCard";
+import {AnimatePresence, motion} from "framer-motion";
 
 function timestampToDdMm(timestamp: number): string {
     const date = new Date(timestamp);
@@ -35,8 +36,10 @@ export function AveragesOverviewCard({average, generalAverageByDays, averagesByS
     } | null>(null)
 
     return (
+        <motion.div layout whileInView={{opacity: 1}}>
         <Card className="border-t-[2px]">
             <CardHeader>
+                <AnimatePresence key={"header"} mode="popLayout">
                 {(selectedAverage == null) && <>
                     <CardTitle className="text-2xl font-bold">Averages</CardTitle>
                     <h2 className="text-4xl font-bold">{average || "/"}</h2>
@@ -44,54 +47,61 @@ export function AveragesOverviewCard({average, generalAverageByDays, averagesByS
                         The average for the last 14 active school days
                     </CardDescription>
                 </>}
-                {(selectedAverage != null) && <>
-                    <ArrowLeftIcon onClick={() => {
-                        setSelectedAverage(null)
-                    }}/>
-                    <CardTitle>{selectedAverage.name}</CardTitle>
-                    <CardDescription>{selectedAverage.teacher}</CardDescription>
-                    <Gauge color={
-                        (selectedAverage.grade >= 6) ? "text-[hsla(110,51%,44%,1)]" :
-                            (selectedAverage.grade >= 5) ? "text-[hsla(40,51%,44%,1)]" :
-                                "text-[hsla(0,51%,44%,1)]"
-                    } value={selectedAverage.grade}
-                           size={"medium"} showValue={true}/>
-                </>}
+                </AnimatePresence>
             </CardHeader>
             <CardContent>
-                <div className="grid grid-cols-3 grid-rows-1 gap-x-6">
-                    {(selectedAverage == null) && <div className="text-center">
-                        <GradesChart grades={generalAverageByDays.sort(a => a.date).reverse().slice(0, 14)
-                            .map((grade) => {
-                                return {...grade, date: timestampToDdMm(grade.date)}
-                            })} className={"relative top-[50%] transform-[translate(-9%,-50%)]"}/>
-                    </div>
-                    }
-                    {(selectedAverage == null) && <div className="col-span-2">
-                        <h2 className="text-xl font-semibold my-2">Subjects</h2>
-                        <div className="grid grid-cols-2 gap-x-6">
-                            {averagesBySubject.slice(0, 4).map((average) =>
-                                <AverageCard average={average} key={average.id} onArrowClick={() => {
-                                    setSelectedAverage(average)
-                                }}/>
-                            )}
-                        </div>
-                    </div>}
-                    {(selectedAverage == null) && <div className={"grid grid-cols-3 gap-6 col-span-full"}>
-                        {averagesBySubject.slice(4).map((average) =>
-                            <AverageCard average={average} key={average.id} onArrowClick={() => {
-                                setSelectedAverage(average)
-                            }}/>
-                        )}
-                    </div>}
-                    {(selectedAverage != null) && <div>
-                        {selectedAverage.grades.map((grade) =>
-                            <GradeCard grade={grade} key={grade.id}/>
-                        )}
-                    </div>
-                    }
+                <div>
+                        {(selectedAverage == null) && <motion.div><h2 className="text-xl font-semibold col-span-3">Subjects</h2>
+                            <div className="grid grid-cols-3 grid-rows-3 gap-x-6">
+                                <div className="text-center row-span-2">
+                                    <GradesChart grades={generalAverageByDays.sort(a => a.date).reverse().slice(0, 14)
+                                        .map((grade) => {
+                                            return {...grade, date: timestampToDdMm(grade.date)}
+                                        })} className={"relative top-[50%] transform-[translate(-9%,-50%)]"}/>
+                                </div>
+
+                                {averagesBySubject.map((average) =>
+                                    <AverageCard average={average} key={average.id} onArrowClick={() => {
+                                        setSelectedAverage(average)
+                                    }}/>
+                                )}
+                            </div>
+                        </motion.div>}
+                        {(selectedAverage != null) &&
+                            <motion.div layout key={selectedAverage.id} layoutId={selectedAverage.id.toString()}>
+                                <Card className="flex items-center gap-2 px-4 py-2 my-2 border-t border-t-[2px]"
+                                      key={selectedAverage.id}>
+                                    <div className="grid w-full">
+
+                                        <div className="col-start-1 row-start-1 w-full items-center">
+                                            <motion.h1 className={"text-xl font-semibold"}
+                                                       layoutId={`${selectedAverage.id}-name`}
+                                                       layout={"position"}>{selectedAverage.name}</motion.h1>
+                                        </div>
+                                        <div className="col-start-1 row-start-1 w-full flex justify-start">
+                                            <motion.div style={{ rotate: 180 }} layoutId={`${selectedAverage.id}-arrow`}><ChevronRight onClick={() =>
+                                                setSelectedAverage(null)
+                                            }/></motion.div>
+                                        </div>
+                                    </div>
+                                    <motion.h2 className={"text-l font-light"}
+                                               layoutId={`${selectedAverage.id}-teacher`}
+                                               layout>{selectedAverage.teacher}</motion.h2>
+                                    <Gauge color={
+                                    (selectedAverage.grade >= 6) ? "text-[hsla(110,51%,44%,1)]" :
+                                        (selectedAverage.grade >= 5) ? "text-[hsla(40,51%,44%,1)]" :
+                                            "text-[hsla(0,51%,44%,1)]"
+                                } value={selectedAverage.grade} gradeId={selectedAverage.id}
+                                       size={"medium"} showValue={true}/>
+                                {selectedAverage.grades.map((grade) =>
+                                    <GradeCard grade={grade} key={grade.id}/>
+                                )}
+                                </Card>
+                            </motion.div>
+                        }
                 </div>
             </CardContent>
         </Card>
+        </motion.div>
     )
 }
