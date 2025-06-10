3154d78f0375b7e67ba1910808ecaa301dcb3ab0
Christian <25307474+Chicchi7393@users.noreply.github.com>
2025-06-11 01:34:06 +0200

    added more data for teacher scrap page

--- backend/src/routes/teachers/classes.ts
+++ backend/src/routes/teachers/classes.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import { db } from "@/db/index.js";
-import { eq } from "drizzle-orm";
+import { eq, count } from "drizzle-orm";
 import { classes } from "@/db/schema/classes.js";
 import { teacherClasses } from "@/db/schema/teacherClasses.js";
+import { teachers } from "@/db/schema/teachers.js";
+import { users } from "@/db/schema/users.js";
+import {students} from "@/db/schema/students.js";
 
 export default async function () {
   const router = new Hono().basePath("/api/v1/teachers");
@@ -14,10 +17,22 @@ export default async function () {
       .select({
         classId: classes.classId,
         className: classes.className,
+        schoolYear: classes.schoolYear,
+        coordinator: users.name,
+        studentCount: count(students.studentId),
       })
       .from(teacherClasses)
       .innerJoin(classes, eq(teacherClasses.classId, classes.classId))
-      .where(eq(teacherClasses.teacherId, user.userId));
+      .innerJoin(teachers, eq(classes.coordinatorTeacherId, teachers.teacherId))
+      .innerJoin(users, eq(teachers.userId, users.userId))
+      .leftJoin(students, eq(students.classId, classes.classId))
+      .where(eq(teacherClasses.teacherId, user.userId))
+      .groupBy(
+        classes.classId,
+        classes.className,
+        classes.schoolYear,
+        users.name
+      );
 
     return c.json({ allClasses });
   });
