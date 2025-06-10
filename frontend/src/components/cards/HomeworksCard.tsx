9d9746e5930ed62680ec310b441e3fc15d75d480
Christian <25307474+Chicchi7393@users.noreply.github.com>
2025-06-11 01:02:12 +0200

    added expanded homeworks card

--- frontend/src/components/cards/HomeworksCard.tsx
+++ frontend/src/components/cards/HomeworksCard.tsx
@@ -1,8 +1,12 @@
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import {Homework} from "@/types/homework";
-import HomeworkCard from "@/components/cards/HomeworkCard";
+import HomeworkCard from "@/components/cards/homeworks/HomeworkCard";
+import HomeworkExpandedCard from "@/components/cards/homeworks/HomeworkExpandedCard";
+import {useState} from "react";
 
 export function HomeworksCard({homeworks}: { homeworks: Homework[] }) {
+    const [selectedHomework, setSelectedHomework] = useState<Homework | null>(null)
+
     return (
         <Card className="border-t-[2px]">
             <CardHeader>
@@ -12,7 +16,10 @@ export function HomeworksCard({homeworks}: { homeworks: Homework[] }) {
                 </CardDescription>
             </CardHeader>
             <CardContent>
-                {homeworks.map((homework: Homework) => <HomeworkCard homework={homework} key={homework.homeworkId}/>)}
+                { selectedHomework == null && homeworks.map((homework: Homework) => <HomeworkCard homework={homework} onArrowClick={() => {
+                    setSelectedHomework(homework)
+                }}/>)}
+                {(selectedHomework != null) && <HomeworkExpandedCard homework={selectedHomework} goBack={() => setSelectedHomework(null)} /> }
             </CardContent>
         </Card>
     )
